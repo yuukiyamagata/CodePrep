eae128c1e8ca75c9d80f4e6eb6dbae6f466f57d6
yuki yamagata <yshanxing4@gmail.com>
2023-02-11 17:00:22 +0900

    007removeOdd

---
+++ js/007removeOdd.js
@@ -0,0 +1,15 @@
+const removeOdd = (array) => {
+  // 配列から要素を取り除くときはバックループを使用すると楽
+  for(let i = array.length - 1; i >= 0; i--){
+    if( array[i] % 2 !== 0) array.splice(i, 1);
+  }
+  return array
+}
+
+const result1 = removeOdd([1, 2, 3, 4, 5])
+const result2 = removeOdd([10, 25, 37, 42, 54])
+const result3 = removeOdd([3, 5, 7])
+
+console.log(result1) // -> [2, 4]
+console.log(result2) // -> [10, 42, 54]
+console.log(result3) // -> []
